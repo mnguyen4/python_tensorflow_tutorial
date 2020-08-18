4c88b9acd3e1749ec78bb9ba36e70414d1a92fe6
mnguyen4 <minov88@gmail.com>
2020-08-17 22:40:52 -0700

    Added sizing to demonstrate overfitting.

--- src/example-overfit-underfit.py
+++ src/example-overfit-underfit.py
@@ -92,3 +92,47 @@ def compile_and_fit(model, name, optimizer=None, max_epochs=10000):
         verbose=0
     )
     return history
+
+tiny_model = tf.keras.Sequential([
+    layers.Dense(16, activation='elu', input_shape=(FEATURES,)),
+    layers.Dense(1)
+])
+
+size_histories = {}
+size_histories['Tiny'] = compile_and_fit(tiny_model, 'sizes/Tiny')
+
+plotter = tfdocs.plots.HistoryPlotter(metric='binary_crossentropy', smoothing_std=10)
+plotter.plot(size_histories)
+plt.ylim([0.5, 0.7])
+plt.show()
+
+small_model = tf.keras.Sequential([
+    layers.Dense(16, activation='elu', input_shape=(FEATURES, )),
+    layers.Dense(16, activation='elu'),
+    layers.Dense(1)
+])
+size_histories['Small'] = compile_and_fit(small_model, 'sizes/Small')
+
+medium_model = tf.keras.Sequential([
+    layers.Dense(64, activation='elu', input_shape=(FEATURES,)),
+    layers.Dense(64, activation='elu'),
+    layers.Dense(64, activation='elu'),
+    layers.Dense(1)
+])
+size_histories['Medium'] = compile_and_fit(medium_model, 'sizes/Medium')
+
+large_model = tf.keras.Sequential([
+    layers.Dense(512, activation='elu', input_shape=(FEATURES,)),
+    layers.Dense(512, activation='elu'),
+    layers.Dense(512, activation='elu'),
+    layers.Dense(512, activation='elu'),
+    layers.Dense(1)
+])
+size_histories['Large'] = compile_and_fit(large_model, 'sizes/Large')
+
+plotter.plot(size_histories)
+a = plt.xscale('log')
+plt.xlim([5, max(plt.xlim())])
+plt.ylim([0.5, 0.7])
+plt.xlabel("Epochs [Log Scale]")
+plt.show()
