383d0f3e35b4d3b851dbd11522df8743c48773e6
mnguyen4 <minov88@gmail.com>
2020-09-01 22:27:14 -0700

    Added hypertuning example.

--- src/example-keras-tuner.py
+++ src/example-keras-tuner.py
@@ -14,10 +14,31 @@ def model_builder(hp):
 
     hp_units = hp.Int('units', min_value=32, max_value=512, step=32)
     model.add(keras.layers.Dense(units=hp_units, activation='relu'))
-    model.add(keras.layers.Desnse(10))
+    model.add(keras.layers.Dense(10))
 
     hp_learning_rate = hp.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4])
 
     model.compile(optimizer=keras.optimizers.Adam(learning_rate=hp_learning_rate), loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
 
     return model
+
+print("================== HyperTuning ==================")
+
+tuner = kt.Hyperband(model_builder, objective='val_accuracy', max_epochs=10, factor=3, directory='my_dir', project_name='intro_to_kt')
+
+class ClearTrainingOutput(tf.keras.callbacks.Callback):
+    def on_train_end(*args, **kwargs):
+        IPython.display.clear_output(wait=True)
+
+tuner.search(img_train, label_train, epochs=10, validation_data=(img_test, label_test), callbacks=[ClearTrainingOutput()])
+
+best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
+
+print(f"""
+The hyperparameter search is complete. The optimal number of units in the first densely-connected
+layer is {best_hps.get('units')} and the optimal learning rate for the optimizer
+is {best_hps.get('learning_rate')}.
+""")
+
+model = tuner.hypermodel.build(best_hps)
+model.fit(img_train, label_train, epochs=10, validation_data=(img_test, label_test))
