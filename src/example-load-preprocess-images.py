8cd663bc4d3a96d0e691a7c979f959fa4416ef15
mnguyen4 <minov88@gmail.com>
2020-12-17 22:50:03 -0700

    Added more finetuning to dataset performance.

--- src/example-load-preprocess-images.py
+++ src/example-load-preprocess-images.py
@@ -93,3 +93,66 @@ list_ds = tf.data.Dataset.list_files(str(data_dir/'*/*'), shuffle=False)
 list_ds = list_ds.shuffle(image_count, reshuffle_each_iteration=False)
 for f in list_ds.take(5):
     print(f.numpy())
+
+# get the class names of the data set
+class_names = np.array(sorted([item.name for item in data_dir.glob("*") if item.name != "LICENSE.txt"]))
+print(class_names)
+
+# get the training data
+val_size = int(image_count*0.2)
+train_ds = list_ds.skip(val_size)
+val_ds = list_ds.take(val_size)
+
+# print out the length of the data
+print(tf.data.experimental.cardinality(train_ds).numpy())
+print(tf.data.experimental.cardinality(val_ds).numpy())
+
+def get_label(file_path):
+    # convert path to path components
+    parts = tf.strings.split(file_path, os.path.sep)
+    # get the class-directory
+    one_hot = parts[-2] == class_names
+    # integer encode label
+    return tf.argmax(one_hot)
+
+def decode_img(img):
+    # convert string to tensor
+    img = tf.image.decode_jpeg(img, channels=3)
+    # resize
+    return tf.image.resize(img, [img_height, img_width])
+
+def process_path(file_path):
+    label = get_label(file_path)
+    # load data as string
+    img = tf.io.read_file(file_path)
+    img = decode_img(img)
+    return img, label
+
+# load and process images in parallel
+train_ds = train_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+val_ds = val_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+
+for image, label in train_ds.take(1):
+    print("Image shape: ", image.numpy().shape)
+    print("Lable: ", label.numpy())
+
+# configure dataset for performance using shuffle and batch
+def configure_for_performance(ds):
+    ds = ds.cache()
+    ds = ds.shuffle(buffer_size=1000)
+    ds = ds.batch(batch_size)
+    ds = ds.prefetch(buffer_size=AUTOTUNE)
+    return ds
+
+train_ds = configure_for_performance(train_ds)
+val_ds = configure_for_performance(val_ds)
+
+image_batch, label_batch = next(iter(train_ds))
+plt.figure(figsize=(10, 10))
+for i in range(9):
+    ax = plt.subplot(3, 3, i+1)
+    plt.imshow(image_batch[i].numpy().astype("uint8"))
+    label = label_batch[i]
+    plt.title(class_names[label])
+    plt.axis("off")
+plt.show()
