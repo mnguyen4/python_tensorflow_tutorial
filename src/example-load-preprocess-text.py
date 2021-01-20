32a6b19d4d837a948cfafea3f10d147de443a28a
mnguyen4 <minov88@gmail.com>
2021-01-20 16:40:23 -0700

    Dataset vectorization and performance optimization.

--- src/example-load-preprocess-text.py
+++ src/example-load-preprocess-text.py
@@ -75,7 +75,7 @@ def binary_vectorize_text(text, label):
     text = tf.expand_dims(text, -1)
     return binary_vectorize_layer(text), label
 
-def int_vextorize_text(text, label):
+def int_vectorize_text(text, label):
     text = tf.expand_dims(text, -1)
     return int_vectorize_layer(text), label
 
@@ -85,4 +85,43 @@ print("Question", first_question)
 print("Label", first_label)
 
 print("'binary' vectorized question:", binary_vectorize_text(first_question, first_label)[0])
-print("'int' vectorized question:", int_vextorize_text(first_question, first_label)[0])
+print("'int' vectorized question:", int_vectorize_text(first_question, first_label)[0])
+
+# int to vocab
+print("1289 ---> ", int_vectorize_layer.get_vocabulary()[1289])
+print("313 ---> ", int_vectorize_layer.get_vocabulary()[313])
+print("Vocabulary size: {}".format(len(int_vectorize_layer.get_vocabulary())))
+
+# apply vectorized text to dataset
+binary_train_ds = raw_train_ds.map(binary_vectorize_text)
+binary_val_ds = raw_val_ds.map(binary_vectorize_text)
+binary_test_ds = raw_test_ds.map(binary_vectorize_text)
+
+int_train_ds = raw_train_ds.map(int_vectorize_text)
+int_val_ds = raw_val_ds.map(int_vectorize_text)
+int_test_ds = raw_test_ds.map(int_vectorize_text)
+
+print("============================ Configure dataset for performance ============================")
+AUTOTUNE = tf.data.AUTOTUNE
+
+def configure_dataset(dataset):
+    return dataset.cache().prefetch(buffer_size=AUTOTUNE)
+
+binary_train_ds = configure_dataset(binary_train_ds)
+binary_val_ds = configure_dataset(binary_val_ds)
+binary_test_ds = configure_dataset(binary_test_ds)
+
+int_train_ds = configure_dataset(int_train_ds)
+int_val_ds = configure_dataset(int_val_ds)
+int_test_ds = configure_dataset(int_test_ds)
+
+# training time
+binary_model = tf.keras.Sequential([layers.Dense(4)])
+binary_model.compile(
+    loss=losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer="adam",
+    metrics=["accuracy"]
+)
+history = binary_model.fit(
+    binary_train_ds, validation_data=binary_val_ds, epochs=10
+)
