134391778422ebbaf99dcd0f5b6505212a774205
mnguyen4 <minov88@gmail.com>
2021-01-22 15:59:46 -0700

    IMDB reviews example.

--- src/example-load-preprocess-text.py
+++ src/example-load-preprocess-text.py
@@ -331,3 +331,63 @@ predicted_labels = tf.argmax(predicted_scores, axis=1)
 for input, label in zip(inputs, predicted_labels):
     print("Question: ", input)
     print("Predicted label: ", label.numpy())
+
+print("============================ Download datasets using TFDS ============================")
+train_ds = tfds.load("imdb_reviews", split="train", batch_size=BATCH_SIZE, shuffle_files=True, as_supervised=True)
+val_ds = tfds.load("imdb_reviews", split="train", batch_size=BATCH_SIZE, shuffle_files=True, as_supervised=True)
+for review_batch, label_batch in val_ds.take(1):
+    for i in range(5):
+        print("Review: ", review_batch[i].numpy())
+        print("Label: ", label_batch[i].numpy())
+
+# prep dataset for training
+vectorize_layer = TextVectorization(max_tokens=VOCAB_SIZE, output_mode="int", output_sequence_length=MAX_SEQUENCE_LENGTH)
+# text only dataset
+train_text = train_ds.map(lambda text, labels: text)
+vectorize_layer.adapt(train_text)
+
+def vectorize_text(text, label):
+    text = tf.expand_dims(text, -1)
+    return vectorize_layer(text), label
+
+train_ds = train_ds.map(vectorize_text)
+val_ds = val_ds.map(vectorize_text)
+# configure datasets for performance
+train_ds = configure_dataset(train_ds)
+val_ds = configure_dataset(val_ds)
+# training time
+model = create_model(vocab_size=VOCAB_SIZE+1, num_labels=1)
+model.summary()
+model.compile(
+    loss=losses.BinaryCrossentropy(from_logits=True),
+    optimizer="adam",
+    metrics=["accuracy"]
+)
+history = model.fit(train_ds, validation_data=val_ds, epochs=3)
+
+loss, accuracy = model.evaluate(val_ds)
+print("Loss: ", loss)
+print("Accuracy: {:2.2%}".format(accuracy))
+
+# export model
+export_model = tf.keras.Sequential([
+    vectorize_layer, model, layers.Activation("sigmoid")
+])
+export_model.compile(
+    loss=losses.SparseCategoricalCrossentropy(from_logits=False),
+    optimizer="adam",
+    metrics=["accuracy"]
+)
+# 0 = negative review, 1 = positive review
+inputs = [
+    "This is a fantastic movie.",
+    "This is a bad movie.",
+    "This movie was so bad that it was good.",
+    "I will never say yes to watching this movie.",
+    "I like turtles."
+]
+predicted_scores = export_model.predict(inputs)
+predicted_labels = [int(round(x[0])) for x in predicted_scores]
+for input, label in zip(inputs, predicted_labels):
+    print("Question: ", input)
+    print("Predicted label: ", label)
